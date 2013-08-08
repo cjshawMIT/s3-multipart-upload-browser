6b847ede40b0b6e6abc0c772c79fb6dc4cfeea8c
hridayeshgupta <hridayeshiitd@gmail.com>
2013-08-08 19:15:12 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'config.php';
 function sendJson($arr)
 {
     header('Content-Type: application/json');
@@ -8,102 +8,45 @@ function sendJson($arr)
 
 $command = isset($_GET['command']) ? strtolower($_GET['command']) : '';
 
-require 'vendor/autoload.php';
-require 'config.php';
-
-use Aws\Common\Enum\DateFormat;
-use Aws\S3\Model\MultipartUpload\UploadId;
-use Aws\S3\S3Client;
-
-$client = S3Client::factory(array(
-        'key' => AWS_KEY,
-        'secret' => AWS_SECRET
-    ));
-
-function isAllowed()
-{
-//wow, what a validator :P
-//WARNING: this is just a demonstration, convert it to your own need
-    return ($_REQUEST['otherInfo']['user'] == 'user' && $_REQUEST['otherInfo']['pass'] == 'pass');
-}
-
 switch ($command) {
     case 'createmultipartupload': {
-            if (!isAllowed()) {
-                header(' ', true, 403);
-                die('You are not authorized');
-            }
-
-            /* @var $multipartUploadModel UploadId */
-            $model = $client->createMultipartUpload(array(
-                'Bucket' => BUCKET_NAME,
-                'Key' => $_REQUEST['fileInfo']['name'],
-                'ContentType' => $_REQUEST['fileInfo']['type'],
-                'Metadata' => $_REQUEST['fileInfo']
-            ));
-
-            sendJson(array(
-                'uploadId' => $model->get('UploadId'),
-                'key' => $model->get('Key'),
-            ));
+            $file = preg_replace("/[^A-Za-z0-9.]/", '', $_REQUEST['fileInfo']['name']);
+            $file = BASE_S3_PATH.time().$file;
+            $exp = time() + 300;
+    		//init upload
+			$upload_sig = base64_encode(hash_hmac("sha1", "POST\n\n\n$exp\n/".BUCKET_NAME.$file."?uploads", AWS_SECRET, true));
+			$url = "http://s3-ap-southeast-1.amazonaws.com/".BUCKET_NAME.$file."?uploads&".
+			http_build_query(array("Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
+			
+			$res = array("url"=>$url, "key"=>$file);
+            sendJson($res);
             break;
         }
     case 'signuploadpart': {
-            $command = $client->getCommand('UploadPart',
-                array(
-                'Bucket' => BUCKET_NAME,
-                'Key' => $_REQUEST['sendBackData']['key'],
-                'UploadId' => $_REQUEST['sendBackData']['uploadId'],
-                'PartNumber' => $_REQUEST['partNumber'],
-                'ContentLength' => $_REQUEST['contentLength']
-            ));
+		$parts = array();
+		$numParts = $_REQUEST['numParts'];
+		$file = $_REQUEST['key'];
+		$upId = $_REQUEST['uploadId'];
+		$exp = time() + 7200;
+		for($part=1;$part<=$numParts;$part++) {
+			$upload_sig = base64_encode(hash_hmac("sha1", "PUT\n\n\n$exp\n/".BUCKET_NAME.$file."?partNumber=$part&uploadId=$upId", AWS_SECRET, true));
 
-            $request = $command->prepare();
-            // This dispatch commands wasted a lot of my times :'(
-            $client->dispatch('command.before_send', array('command' => $command));
-            $request->removeHeader('User-Agent');
-            $request->setHeader('x-amz-date', gmdate(DateFormat::RFC2822));
-            // This dispatch commands wasted a lot of my times :'(
-            $client->dispatch('request.before_send', array('request' => $request));
+			$url = "http://s3-ap-southeast-1.amazonaws.com/".BUCKET_NAME.$file."?". http_build_query(array("partNumber"=>$part, "uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
 
-            sendJson(array(
-                'url' => $request->getUrl(),
-                'authHeader' => (string) $request->getHeader('Authorization'),
-                'dateHeader' => (string) $request->getHeader('x-amz-date'),
-            ));
-            break;
-        }
-    case 'completemultipartupload': {
-            $partsModel = $client->listParts(array(
-                'Bucket' => BUCKET_NAME,
-                'Key' => $_REQUEST['sendBackData']['key'],
-                'UploadId' => $_REQUEST['sendBackData']['uploadId'],
-            ));
+			$parts[] = array("index"=>$part-1, "url"=>$url);
+		}
+		$upload_sig = base64_encode(hash_hmac("sha1", "DELETE\n\n\n$exp\n/".BUCKET_NAME.$file."?uploadId=$upId", AWS_SECRET, true));
 
-            $model = $client->completeMultipartUpload(array(
-                'Bucket' => BUCKET_NAME,
-                'Key' => $_REQUEST['sendBackData']['key'],
-                'UploadId' => $_REQUEST['sendBackData']['uploadId'],
-                'Parts' => $partsModel['Parts'],
-            ));
+		$aborturl = "http://s3-ap-southeast-1.amazonaws.com/".BUCKET_NAME.$file."?". http_build_query(array("uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
+		
+		$exp = time() + 18000;
+		$upload_sig = base64_encode(hash_hmac("sha1", "POST\n\ntext/plain; charset=UTF-8\n$exp\n/".BUCKET_NAME.$file."?uploadId=$upId", AWS_SECRET, true));
 
-            sendJson(array(
-                'success' => true
-            ));
-            break;
-        }
-    case 'abortmultipartupload': {
-            $model = $client->abortMultipartUpload(array(
-                'Bucket' => BUCKET_NAME,
-                'Key' => $_REQUEST['sendBackData']['key'],
-                'UploadId' => $_REQUEST['sendBackData']['uploadId']
-            ));
-
-            sendJson(array(
-                'success' => true
-            ));
-            break;
-        }
+		$completeurl = "http://s3-ap-southeast-1.amazonaws.com/".BUCKET_NAME.$file."?". http_build_query(array("uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
+		
+		sendJson(array("parts"=>$parts, "abort"=>$aborturl, "complete"=>$completeurl));
+		break;
+       }
     default: {
             header(' ', true, 404);
             die('Command not understood');
